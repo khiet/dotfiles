a71d7585c0de66e074bb8157354f4f4770071160
Khiet Le <khiet.le@ehochef.com>
2021-01-26 10:45:56 +0000

    adjust script to open file in github

--- scripts/git_open_file_in_github.sh
+++ scripts/git_open_file_in_github.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-open $(git remote -v | awk '{print $2}' | sed -E 's#(git@|git://)#https://#' | sed 's#github.com:#github.com/#' | sed 's#\.git#/#' | head -n1)/blob/master/$1
+open $(git remote -v | awk '{print $2}' | sed -E 's#(git@|git://)#https://#' | sed 's#github.com:#github.com/#' | sed 's#\.git#/#' | head -n1)blob/master/$1
