7a39434d0b05fd995d20223b92ed48e41c29b705
Khiet Le <khiet@outlook.com>
2021-05-26 21:44:10 +0100

    add guard clause to check if file exists already and add template for the new file

--- scripts/create_spec_file.rb
+++ scripts/create_spec_file.rb
@@ -6,6 +6,20 @@ file_path = ARGV[0]
 spec_file_path = file_path.sub(%r{(app/)(.*).rb}, 'spec/\2_spec.rb')
 spec_pathname =  Pathname(spec_file_path)
 
+exit(0) if File.exist?(spec_pathname)
+
 FileUtils.mkdir_p spec_pathname.dirname
 FileUtils.cd spec_pathname.dirname
 FileUtils.touch spec_pathname.basename
+
+template = <<~HERE
+  require 'rails_helper'
+
+  RSpec.describe OBJECT_UNDER_TEST do
+
+  end
+HERE
+
+File.open(spec_pathname, 'w') do |file|
+  file.write(template)
+end
