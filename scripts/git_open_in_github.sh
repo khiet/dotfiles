7edeaa5cdecae85fe4885676617f714141d2a275
Khiet Le <khiet@outlook.com>
2022-04-29 11:46:05 +0100

    fix git_open_in_github to use origin

--- scripts/git_open_in_github.sh
+++ scripts/git_open_in_github.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 alias urlencode='node -e "console.log(encodeURIComponent(process.argv[1]))"'
-open $(git remote -v | awk '{print $2}' | sed -E 's#(git@|git://)#https://#' | sed 's#github.com:#github.com/#' | sed 's#\.git#/#' | head -n1)compare/$(urlencode $(git symbolic-ref --short HEAD))
+open $(git remote -v | grep origin | awk '{print $2}' | sed -E 's#(git@|git://)#https://#' | sed 's#github.com:#github.com/#' | sed 's#\.git#/#' | head -n1)compare/$(urlencode $(git symbolic-ref --short HEAD))
