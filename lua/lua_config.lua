babc4e143f2ad4abcd1ff8cdef7d8bd9a272af1f
Khiet Le <khiet@outlook.com>
2022-09-18 19:59:56 +0100

    add gt and gi

--- lua/lua_config.lua
+++ lua/lua_config.lua
@@ -29,6 +29,8 @@ local on_attach = function(client, bufnr)
   local bufopts = { noremap=true, silent=true, buffer=bufnr }
   vim.keymap.set('n', '<leader>f', vim.lsp.buf.formatting, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
   vim.keymap.set('n', 'ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
 end
