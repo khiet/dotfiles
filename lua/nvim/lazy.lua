c2ac24c685443d4705d4bcbf79acad09d4410b9c
Khiet Le <khiet@outlook.com>
2023-04-15 17:15:52 +0100

    add treesitter

--- lua/nvim/lazy.lua
+++ lua/nvim/lazy.lua
@@ -13,7 +13,8 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({ 
   { 'nvim-telescope/telescope.nvim', tag = '0.1.1', dependencies = { 'nvim-lua/plenary.nvim' } },
-  'Mofiqul/dracula.nvim'
+  'Mofiqul/dracula.nvim',
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" }
 })
 
 vim.cmd('colorscheme dracula')
