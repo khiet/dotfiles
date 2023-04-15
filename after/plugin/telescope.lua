5e71742105f6f7b6f10f66bcd1641bbe8876f9ed
Khiet Le <khiet@outlook.com>
2023-04-15 19:22:37 +0100

    update telescope config

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,16 @@
+require('telescope').setup({
+  defaults = {
+    sorting_strategy = 'ascending',
+    layout_config = {
+      prompt_position = 'top',
+    },
+  },
+  pickers = {
+    find_files = {
+      find_command = { "rg", "--files", "--sort", "path" },
+    },
+  },
+})
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-f>', builtin.find_files, {})
--- vim.keymap.set('n', '<C-g>', builtin.git_files, {})
