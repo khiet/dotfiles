ee2d4466960828dc08c9f955d960b4521e9a6933
Khiet Le <khiet@outlook.com>
2023-04-16 10:17:53 +0100

    add delete_trailing_whitespaces

--- lua/nvim/init.lua
+++ lua/nvim/init.lua
@@ -103,3 +103,18 @@ vim.g.vim_markdown_conceal = 0
 
 -- vim-argwrap
 vim.keymap.set('n', 'gS', vim.cmd.ArgWrap, { silent = true, noremap = true })
+
+-- functions
+function delete_trailing_whitespaces()
+  local s = vim.fn.getreg('/')
+  local l = vim.fn.line('.')
+  local c = vim.fn.col('.')
+
+  vim.cmd('%s/\\s\\+$//e')
+
+  vim.fn.setreg('/', s)
+  vim.fn.cursor(l, c)
+end
+
+vim.keymap.set('n', '<leader>td', delete_trailing_whitespaces, { noremap = true })
+-- vim.keymap.set('n', '<leader>cq', replace_curly_quotes, { noremap = true })
