273d35139eb75f6cf89d07c965c283016dff1be6
Khiet Le <khiet@outlook.com>
2023-04-16 17:15:37 +0100

    implement functions

--- lua/nvim/functions.lua
+++ lua/nvim/functions.lua
@@ -1,96 +1,57 @@
 function delete_trailing_whitespaces()
-  local s = vim.fn.getreg('/')
-  local l = vim.fn.line('.')
-  local c = vim.fn.col('.')
-
   vim.cmd('%s/\\s\\+$//e')
+end
 
-  vim.fn.setreg('/', s)
-  vim.fn.cursor(l, c)
+function replace_curly_quotes()
+  if has_character('’') or has_character('‘') then
+    vim.cmd([[%s/[’‘]/'/g]])
+  end
+
+  if has_character('”') or has_character('“') then
+    vim.cmd([[%s/[”“]/"/g]])
+  end
+end
+
+function has_character(char)
+  local character = char
+
+  local current_buffer = vim.api.nvim_get_current_buf()
+  local buffer_contents = vim.api.nvim_buf_get_lines(current_buffer, 0, -1, false)
+
+  for _, line in ipairs(buffer_contents) do
+    if string.find(line, character, 1, true) ~= nil then
+      return true
+    end
+  end
+
+  return false
 end
 
 function search_dictionary()
   os.execute('open "https://dictionary.cambridge.org/dictionary/english/'..vim.fn.expand('<cword>')..'"')
 end
 
+function create_spec_file()
+  local cmd = '!ruby ~/dotfiles/scripts/create_spec_file.rb ' .. vim.fn.expand("%:p")
+  vim.api.nvim_command("silent execute '" .. cmd .. "'")
+end
+
+function run_script()
+  local filetype = vim.bo.filetype
+
+  if filetype == 'javascript' then
+    vim.fn.VimuxRunCommand("node " .. vim.fn.bufname("%"))
+  elseif filetype == 'ruby' then
+    vim.fn.VimuxRunCommand("ruby -w " .. vim.fn.bufname("%"))
+  elseif filetype == 'rust' then
+    vim.fn.VimuxRunCommand("cargo run")
+  elseif filetype == 'sh' then
+    vim.fn.VimuxRunCommand("./" .. vim.fn.bufname("%"))
+  end
+end
+
 vim.keymap.set('n', '<leader>td', delete_trailing_whitespaces, { noremap = true })
 vim.keymap.set('n', '<leader>cd', search_dictionary, { noremap = true })
--- vim.keymap.set('n', '<leader>cq', replace_curly_quotes, { noremap = true })
-
--- " " -----------------------------------------------
--- "    function
--- " -----------------------------------------------
--- function IsAJavascript()
---   return index(['javascript', 'typescript', 'typescriptreact', 'html', 'json'], &filetype) != -1
--- endfunction
---
--- function! ReplaceCurlyQuotes()
---   let _s=@/
---   let l = line(".")
---   let c = col(".")
---   %s/[’‘]/'/g
---   %s/[”“]/"/g
---   let @/=_s
---   call cursor(l, c)
--- endfunction
---
--- " ctags: http://vim.wikia.com/wiki/Autocmd_to_update_ctags_file
--- function! RunCtags()
---   let cwd = getcwd()
---   let tagfilename = cwd . "/tags"
---   let cmd = 'ctags -R -f ' . tagfilename
---   let resp = system(cmd)
--- endfunction
---
--- function! AddDebugBreakpoint()
---   if IsAJavascript()
---     r!echo 'debugger;'
---     write
---   elseif index(['ruby', 'haml'], &filetype) != -1
---     r!echo "require 'debug'; debugger"
---     write
---   endif
--- endfunction
---
--- function! RunScript()
---   if &filetype == 'javascript'
---     call VimuxRunCommand("node " . bufname("%"))
---   elseif &filetype == 'ruby'
---     call VimuxRunCommand("ruby -w " . bufname("%"))
---   elseif &filetype == 'rust'
---     call VimuxRunCommand("cargo run")
---   elseif &filetype == 'sh'
---     call VimuxRunCommand("./" . bufname("%"))
---   endif
--- endfunction
---
--- " http://vimcasts.org/episodes/tidying-whitespace/
--- function! DeleteTrailingWhitespaces()
---   " Preparation: save last search, and cursor position.
---   let _s=@/
---   let l = line(".")
---   let c = col(".")
---   " do the business:
---   %s/\s\+$//e
---   " clean up: restore previous search history, and cursor position
---   let @/=_s
---   call cursor(l, c)
--- endfunction
---
--- function! CreateSpecFile()
---   silent execute '!ruby ~/dotfiles/scripts/create_spec_file.rb' expand("%:p")
--- endfunction
---
--- function! ConsoleLog()
---   execute "normal! yiwo" . "console.log" . "('')\<C-[>F';a\<C-[>pf'a, \<C-[>p<CR>"
--- endfunction
---
--- " -----------------------------------------------
---
--- nnoremap <silent> <leader>cd :call AddDebugBreakpoint()<CR>
--- nnoremap <silent> <leader>cl :call ConsoleLog()<CR>
--- nnoremap <silent> <leader>cq :call ReplaceCurlyQuotes()<CR>
--- nnoremap <silent> <leader>ct :call RunCtags()<CR>
--- nnoremap <silent> <leader>cs :call RunScript()<CR>
--- nnoremap <silent> <leader>rc :call CreateSpecFile()<CR>
--- nnoremap <silent> <leader>td :call DeleteTrailingWhitespaces()<CR>
+vim.keymap.set('n', '<leader>cq', replace_curly_quotes, { noremap = true })
+vim.keymap.set('n', '<leader>rc', create_spec_file, { noremap = true })
+vim.keymap.set('n', '<leader>cs', run_script, { noremap = true })
