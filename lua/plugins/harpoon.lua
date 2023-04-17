450bec4a56adc0372c4b2d289299c9b3cd385977
Khiet Le <khiet@outlook.com>
2023-04-17 11:43:11 +0100

    remap harpoon to <C-e> as <C-m> is Enter

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,13 +1,13 @@
 return {
   {
     "ThePrimeagen/harpoon",
-    keys = { "<leader>a", "<C-m>" },
+    keys = { "<leader>a", "<C-e>" },
     config = function()
       local mark = require("harpoon.mark")
       local ui = require("harpoon.ui")
 
       vim.keymap.set("n", "<leader>a", mark.add_file)
-      vim.keymap.set("n", "<C-m>", ui.toggle_quick_menu)
+      vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
     end
   }
 }
