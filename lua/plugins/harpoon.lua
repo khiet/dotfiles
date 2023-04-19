41aec981181c4ffa9ba5428dd370a29d6d5f893c
Khiet Le <khiet@outlook.com>
2023-04-19 12:09:00 +0100

    increase harpoon width

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -3,6 +3,12 @@ return {
     "ThePrimeagen/harpoon",
     keys = { "<leader>a", "<C-e>" },
     config = function()
+      require("harpoon").setup({
+        menu = {
+          width = 120
+        }
+      })
+
       local mark = require("harpoon.mark")
       local ui = require("harpoon.ui")
 
