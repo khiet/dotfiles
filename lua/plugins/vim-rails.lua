88e5bc8d2b791c60e3af1f885f6bf5b7af14a3c2
Khiet Le <khiet@outlook.com>
2023-04-23 09:46:10 +0100

    update vim-rails

--- lua/plugins/vim-rails.lua
+++ lua/plugins/vim-rails.lua
@@ -1,22 +1,23 @@
 return {
   {
     "tpope/vim-rails",
-    keys = { '<leader>rr', '<leader>ra' },
+    ft = { "ruby" },
     config = function()
       vim.keymap.set('n', '<leader>rr', vim.cmd.R, { noremap = true })
       vim.keymap.set('n', '<leader>ra', vim.cmd.A, { noremap = true })
+
+      vim.g.rails_projections = {
+        ["app/controllers/*_controller.rb"] = {
+          alternate = {
+            "spec/requests/{}_spec.rb",
+          },
+        },
+        ["spec/requests/*_spec.rb"] = {
+          alternate = {
+            "app/controllers/{}_controller.rb",
+          },
+        },
+      }
     end
   }
 }
-
--- let g:rails_projections = {
--- \   "app/controllers/*_controller.rb": {
--- \     "test": ["spec/requests/{}_request_spec.rb", "spec/requests/{}_spec.rb"]
--- \   },
--- \   "spec/requests/*_spec.rb": {
--- \     "alternate": "app/controllers/{}_controller.rb"
--- \   },
--- \   "spec/requests/*_request_spec.rb": {
--- \     "alternate": "app/controllers/{}_controller.rb"
--- \   },
--- \ }
