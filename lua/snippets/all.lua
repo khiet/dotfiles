610931fef6d171d2db5ecd67b1f705c2c54336d6
Khiet Le <khiet@outlook.com>
2023-04-27 19:48:26 +0100

    add snippets

---
+++ lua/snippets/all.lua
@@ -0,0 +1,23 @@
+-- https://github.com/L3MON4D3/LuaSnip/blob/master/Examples/snippets.lua
+-- https://github.com/rafamadriz/friendly-snippets/tree/main/snippets
+
+local luasnip = require 'luasnip'
+
+local s = luasnip.snippet
+local t = luasnip.text_node
+local i = luasnip.insert_node
+
+luasnip.add_snippets("all", {
+  s(
+    "debug",
+    {t("require 'debug'; debugger")}
+  ),
+  s(
+    "pry",
+    {t("require 'pry'; binding.pry")}
+  ),
+  s(
+    "cl",
+    {t("console.log('"), i(1, "message"), t("');")}
+  ),
+})
