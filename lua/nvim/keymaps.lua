af6630e20d1a52d1be550422bcb27e5ced84a5ca
Khiet Le <khiet@outlook.com>
2023-04-28 07:32:43 +0100

    refactor keymaps

--- lua/nvim/keymaps.lua
+++ lua/nvim/keymaps.lua
@@ -1,60 +1,62 @@
 vim.g.mapleader = " "
 
+local set = vim.keymap.set
+
 -- disable command-line window
-vim.keymap.set('n', 'q:', '<NOP>', { noremap = true })
+set('n', 'q:', '<NOP>', { noremap = true })
 -- disable ex-mode
-vim.keymap.set('n', 'Q', '<NOP>', { noremap = true })
+set('n', 'Q', '<NOP>', { noremap = true })
 
-vim.keymap.set('n', 'Q', 'q', { noremap = true })
-vim.keymap.set('n', 'q', '<NOP>', { noremap = true })
+set('n', 'Q', 'q', { noremap = true })
+set('n', 'q', '<NOP>', { noremap = true })
 
-vim.keymap.set('n', '<Up>', '<NOP>', { noremap = true })
-vim.keymap.set('n', '<Down>', '<NOP>', { noremap = true })
-vim.keymap.set('n', '<Left>', '<NOP>', { noremap = true })
-vim.keymap.set('n', '<Right>', '<NOP>', { noremap = true })
+set('n', '<Up>', '<NOP>', { noremap = true })
+set('n', '<Down>', '<NOP>', { noremap = true })
+set('n', '<Left>', '<NOP>', { noremap = true })
+set('n', '<Right>', '<NOP>', { noremap = true })
 
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { noremap = true })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { noremap = true })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { noremap = true })
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { noremap = true })
+set('n', '<C-j>', '<C-w><C-j>', { noremap = true })
+set('n', '<C-k>', '<C-w><C-k>', { noremap = true })
+set('n', '<C-l>', '<C-w><C-l>', { noremap = true })
+set('n', '<C-h>', '<C-w><C-h>', { noremap = true })
 
-vim.keymap.set('n', '<leader>w', vim.cmd.w, { noremap = true })
-vim.keymap.set('n', '<leader>x', vim.cmd.x, { noremap = true })
-vim.keymap.set('n', '<leader>q', vim.cmd.q, { noremap = true })
-vim.keymap.set('n', '<leader>e', ":e!<CR>", { noremap = true })
-vim.keymap.set('n', '<leader>Q', ":q!<CR>", { noremap = true })
+set('n', '<leader>w', vim.cmd.w, { noremap = true })
+set('n', '<leader>x', vim.cmd.x, { noremap = true })
+set('n', '<leader>q', vim.cmd.q, { noremap = true })
+set('n', '<leader>e', ":e!<CR>", { noremap = true })
+set('n', '<leader>Q', ":q!<CR>", { noremap = true })
 
 -- http://vim.wikia.com/wiki/Replace_a_word_with_yanked_text
-vim.keymap.set('x', 'p', [[p:let @+=@0<CR>:let @"=@0<CR>]], { silent = true, noremap = true })
+set('x', 'p', [[p:let @+=@0<CR>:let @"=@0<CR>]], { silent = true, noremap = true })
 
 -- switch between the last two files
-vim.keymap.set('n', '<leader><leader>', '<C-^>', { noremap = true })
+set('n', '<leader><leader>', '<C-^>', { noremap = true })
 
-vim.keymap.set({ 'n', 'v' }, '<<', '<gv', { noremap = true })
-vim.keymap.set({ 'n', 'v' }, '>>', '>gv', { noremap = true })
+set({ 'n', 'v' }, '<<', '<gv', { noremap = true })
+set({ 'n', 'v' }, '>>', '>gv', { noremap = true })
 
-vim.keymap.set('n', '<leader>=', "<C-w>=", { noremap = true })
+set('n', '<leader>=', "<C-w>=", { noremap = true })
 
-vim.keymap.set('n', '[b', vim.cmd.bp, { silent = true, noremap = true })
-vim.keymap.set('n', ']b', vim.cmd.bn, { silent = true, noremap = true })
-vim.keymap.set('n', '<leader>bd', vim.cmd.bd, { noremap = true })
+set('n', '[b', vim.cmd.bp, { silent = true, noremap = true })
+set('n', ']b', vim.cmd.bn, { silent = true, noremap = true })
+set('n', '<leader>bd', vim.cmd.bd, { noremap = true })
 
-vim.keymap.set('n', '<leader>h', vim.cmd.noh, { noremap = true })
+set('n', '<leader>h', vim.cmd.noh, { noremap = true })
 
-vim.keymap.set({ 'n', 'v' }, '<leader>s', [[:s//]], { noremap = true })
-vim.keymap.set('n', '<leader>s', [[:%s//]], { noremap = true })
-vim.keymap.set("n", "<leader>S", [[:%s/\<<C-r><C-w>\>/]], { noremap = true })
+set({ 'n', 'v' }, '<leader>s', [[:s//]], { noremap = true })
+set('n', '<leader>s', [[:%s//]], { noremap = true })
+set("n", "<leader>S", [[:%s/\<<C-r><C-w>\>/]], { noremap = true })
 
 -- https://vi.stackexchange.com/a/22889
-vim.keymap.set('n', '<leader>k', [[<Cmd>let @/='\<'.expand('<cword>').'\>'<bar>set hlsearch<CR>]], { noremap = true })
+set('n', '<leader>k', [[<Cmd>let @/='\<'.expand('<cword>').'\>'<bar>set hlsearch<CR>]], { noremap = true })
 
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set({ "n", "v" }, "K", [[:s/ /\r/g<CR>]])
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true })
+set("n", "J", "mzJ`z")
+set({ "n", "v" }, "K", [[:s/ /\r/g<CR>]])
+set("n", "<C-d>", "<C-d>zz", { noremap = true })
+set("n", "<C-u>", "<C-u>zz", { noremap = true })
 
-vim.keymap.set("n", "<leader>ev", ':e <C-R>=expand($HOME."/dotfiles/")<CR><CR>', { noremap = true })
-vim.keymap.set("n", "<leader>ez", ':e <C-R>=expand($HOME."/dotfiles/_zshrc")<CR><CR>', { noremap = true })
-vim.keymap.set("n", "<leader>et", ':e <C-R>=expand($HOME."/dotfiles/_tmux.conf")<CR><CR>', { noremap = true })
+set("n", "<leader>ev", ':e <C-R>=expand($HOME."/dotfiles/")<CR><CR>', { noremap = true })
+set("n", "<leader>ez", ':e <C-R>=expand($HOME."/dotfiles/_zshrc")<CR><CR>', { noremap = true })
+set("n", "<leader>et", ':e <C-R>=expand($HOME."/dotfiles/_tmux.conf")<CR><CR>', { noremap = true })
 vim.cmd([[au BufEnter,BufWinEnter _zshrc set filetype=zsh]])
 vim.cmd([[au BufEnter,BufWinEnter _tmux.conf set filetype=tmux]])
