44f27f52a595507afc2cfeddb59a2e7b7f2ba388
Khiet Le <khiet@outlook.com>
2023-04-28 15:03:15 +0100

    organise lsp settings

--- lua/nvim/lazy.lua
+++ lua/nvim/lazy.lua
@@ -11,7 +11,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins", {
+require("lazy").setup({
+  { import = "plugins" },
+  { import = "plugins.lsp" },
   change_detection = {
     enabled = true,
     notify = false,
