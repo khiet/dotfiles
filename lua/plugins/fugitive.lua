4063c03b8c10c7f5a1e264d476700610c1ca53f9
Khiet Le <khiet@outlook.com>
2023-06-02 17:02:15 +0100

    add GBrowse mappings

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -4,6 +4,7 @@ return {
     dependencies = { "tpope/vim-rhubarb" },
     init = function()
       vim.keymap.set("n", "gb", vim.cmd.GBrowse, { silent = true })
+      vim.keymap.set({ "n", "v" }, "gb", ":GBrowse<CR>", { silent = true })
     end
   }
 }
