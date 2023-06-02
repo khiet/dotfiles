e56d94ef4d12bd6a8a2f145e954d3c87b1ad4455
Khiet Le <khiet@outlook.com>
2023-06-02 17:02:15 +0100

    use range for GBrowse

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -3,7 +3,8 @@ return {
     "tpope/vim-fugitive",
     dependencies = { "tpope/vim-rhubarb" },
     init = function()
-      vim.keymap.set("n", "gb", vim.cmd.GBrowse, { silent = true })
+      vim.keymap.set({ "n", "v" }, "gb", ":GBrowse<CR>", { silent = true, noremap = true })
+      vim.keymap.set("n", "gb", ":0GBrowse<CR>", { silent = true, noremap = true })
     end
   }
 }
