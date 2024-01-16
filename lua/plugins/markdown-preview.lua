843f7e94cc98bf9a4c8717edf62a411ff1234ef3
Khiet Le <khiet@outlook.com>
2024-01-16 08:52:38 +0000

    update markdown-preview config to avoid Lazy U error

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,13 +1,11 @@
 return {
-  {
-    "iamcco/markdown-preview.nvim",
-    build = "cd app && npm install",
-    ft = { "markdown" },
-    config = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-      vim.g.mkdp_images_path = (os.getenv("HOME") .. "/notes/images")
+  "iamcco/markdown-preview.nvim",
+  ft = { "markdown" },
+  build = "cd app && yarn install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+    vim.g.mkdp_images_path = (os.getenv("HOME") .. "/notes/images")
 
-      vim.keymap.set('n', '<leader>em', "<Plug>MarkdownPreviewToggle", { noremap = true })
-    end
-  }
+    vim.keymap.set('n', '<leader>em', "<Plug>MarkdownPreviewToggle", { noremap = true })
+  end
 }
