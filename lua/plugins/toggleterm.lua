ccdb36f469f70effa02a1897a4993b9f4153fd33
Khiet Le <khiet@outlook.com>
2024-01-23 21:18:53 +0000

    replace floaterm with toggleterm

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    init = function()
+      require("toggleterm").setup({
+        float_opts = {
+          width = function()
+            return math.floor(vim.go.columns * 0.8)
+          end,
+          height = function()
+            return math.floor(vim.go.lines * 0.9)
+          end,
+          border = 'double'
+        }
+      })
+
+      -- https://github.com/akinsho/toggleterm.nvim?tab=readme-ov-file#custom-terminals
+      local Terminal = require('toggleterm.terminal').Terminal
+      local lazygit  = Terminal:new({
+        cmd = "lazygit",
+        direction = "float",
+        hidden = true
+      })
+
+      function _lazygit_toggle()
+        lazygit:toggle()
+      end
+
+      vim.keymap.set('n', 'gl', "<cmd>lua _lazygit_toggle()<CR>", { silent = true, noremap = true })
+      vim.keymap.set('n', 'tt', ":ToggleTerm direction=float<CR>", { silent = true, noremap = true })
+    end
+  }
+}
