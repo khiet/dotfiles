6eb8d983c0ec2a8d23a77c94bf2a782a29520e70
Khiet Le <khiet@outlook.com>
2024-02-22 09:42:47 +0000

    search hidden

--- zshrc/ripgrep.zsh
+++ zshrc/ripgrep.zsh
@@ -2,5 +2,5 @@ alias g="rg --sort path --hidden"
 alias G="rg --sort path --no-ignore --hidden"
 alias gw="rg --sort path -w --hidden"
 
-function gf { rg --files --sort path | rg --regexp $1 }
-function gF { rg --files --no-ignore --sort path | rg --regexp $1 }
+function gf { rg --files --sort path --hidden | rg --regexp $1 }
+function gF { rg --files --sort path --no-ignore --hidden | rg --regexp $1 }
