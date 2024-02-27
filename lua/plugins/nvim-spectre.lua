7194457e029c5e6898e02c480687f04cc4d9de32
Khiet Le <khiet@outlook.com>
2024-02-27 17:34:23 +0000

    remap to disable send_to_qf for spectre

--- lua/plugins/nvim-spectre.lua
+++ lua/plugins/nvim-spectre.lua
@@ -3,7 +3,15 @@ return {
     "nvim-pack/nvim-spectre",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-      require('spectre').setup({})
+      require('spectre').setup({
+        mapping = {
+          ['send_to_qf'] = {
+            map = "<F12>",
+            cmd = "<cmd>lua require('spectre.actions').send_to_qf()<CR>",
+            desc = "send all items to quickfix"
+          },
+        }
+      })
 
       vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").open()<CR>', {
         desc = "Open Spectre"
