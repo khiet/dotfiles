1816731a38cc87224770cd03499973062d1d26a1
Khiet Le <khiet@outlook.com>
2024-08-22 09:21:26 +0100

    add autocomplete for markdown table

--- lua/snippets/all.lua
+++ lua/snippets/all.lua
@@ -1,23 +1,31 @@
--- https://github.com/L3MON4D3/LuaSnip/blob/master/Examples/snippets.lua
+-- https://github.com/L3MON4D3/ls/blob/master/Examples/snippets.lua
 -- https://github.com/rafamadriz/friendly-snippets/tree/main/snippets
 
-local luasnip = require 'luasnip'
+local ls = require 'luasnip'
 
-local s = luasnip.snippet
-local t = luasnip.text_node
-local i = luasnip.insert_node
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
 
-luasnip.add_snippets("all", {
+ls.add_snippets("all", {
   s(
     "de",
-    {t("require 'debug'; debugger")}
+    { t("require 'debug'; debugger") }
   ),
   s(
     "pr",
-    {t("require 'pry'; binding.pry")}
+    { t("require 'pry'; binding.pry") }
   ),
   s(
     "cl",
-    {t("console.log('"), i(1, "message"), t("');")}
+    { t("console.log('"), i(1, "message"), t("');") }
+  ),
+  s(
+    "ta",
+    ls.text_node({
+      "| Before | After |",
+      "| ------ | ----- |",
+      '| <img src="" width=300 /> | <img src="" width=300 /> |',
+    })
   ),
 })
