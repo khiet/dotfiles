fa161279000e751559a5153bd50224a00992f8d9
Khiet Le <khiet@outlook.com>
2024-09-21 11:32:43 +0100

    update aws ssh alias

--- zshrc/aws.zsh
+++ zshrc/aws.zsh
@@ -1,6 +1,6 @@
 alias aws_ssh='aws ssm start-session --document-name AWS-StartInteractiveCommand --parameters command="bash -l" --target'
 # connect to an instance protected from scale in in the specified auto scaling group
-alias aws_ssh_pi="aws_ssh $(aws autoscaling describe-auto-scaling-instances | jq -r '.AutoScalingInstances[] | select(.ProtectedFromScaleIn == true and .AutoScalingGroupName == "awseb-e-363wrq8hu8-stack-AWSEBAutoScalingGroup-1gC39UdauRcl") | .InstanceId')"
 
 # set ProtectedFromScaleIn to true
 # aws autoscaling set-instance-protection --instance-ids <instance-id> --auto-scaling-group-name <auto-scaling-group-name> --protected-from-scale-in
+# aws_ssh target should be $(aws autoscaling describe-auto-scaling-instances | jq -r '.AutoScalingInstances[] | select(.ProtectedFromScaleIn == true and .AutoScalingGroupName == "awseb-e-363wrq8hu8-stack-AWSEBAutoScalingGroup-1gC39UdauRcl") | .InstanceId')
