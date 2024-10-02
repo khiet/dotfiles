9987b092e07f9df2d304a42509fa5c9e3ed6cee2
Khiet Le <khiet@outlook.com>
2024-10-02 20:14:28 +0100

    map R to refresh in grug-far

--- lua/plugins/grug-far.lua
+++ lua/plugins/grug-far.lua
@@ -4,9 +4,11 @@ return {
     vim.keymap.set('n', '<leader>S', ':GrugFar<CR>', { silent = true, noremap = true })
   end,
   config = function()
+    -- https://github.com/MagicDuck/grug-far.nvim/blob/main/lua/grug-far/opts.lua
     require('grug-far').setup({
       keymaps = {
         qflist = false,
+        refresh = { n = '<localleader>R' },
       }
     });
   end
