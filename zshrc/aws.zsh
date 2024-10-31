2af6120ed6279eb1e5fef309733267d079446340
Khiet Le <khiet@outlook.com>
2024-10-31 08:15:40 +0000

    add aws_ls_ec2 alias

--- zshrc/aws.zsh
+++ zshrc/aws.zsh
@@ -4,3 +4,5 @@ alias aws_ssh='aws ssm start-session --document-name AWS-StartInteractiveCommand
 # set ProtectedFromScaleIn to true
 # aws autoscaling set-instance-protection --instance-ids <instance-id> --auto-scaling-group-name <auto-scaling-group-name> --protected-from-scale-in
 # aws_ssh target should be $(aws autoscaling describe-auto-scaling-instances | jq -r '.AutoScalingInstances[] | select(.ProtectedFromScaleIn == true and .AutoScalingGroupName == "awseb-e-363wrq8hu8-stack-AWSEBAutoScalingGroup-1gC39UdauRcl") | .InstanceId')
+
+alias aws_ls_ec2='aws ec2 describe-instances --filters Name=instance-state-name,Values=running --query "Reservations[*].Instances[*].{Instance:InstanceId,Type:InstanceType,State:State.Name,Name:Tags[?Key==\`Name\`]|[0].Value}" --output table'
