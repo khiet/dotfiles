6f6c3a0740f1289838c72bef3eec487477a0d71e
Khiet Le <khiet@outlook.com>
2025-06-12 12:59:13 +0100

    update treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,8 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = 'master',
+    lazy = false,
     build = ":TSUpdate",
     config = function()
       require('nvim-treesitter.configs').setup({
