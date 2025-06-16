1adc973189f6071ea327ca48be039a7ad238ff63
Khiet Le <khiet@outlook.com>
2025-06-16 14:21:43 +0100

    disable checking for plugins automatically

--- lua/nvim/lazy.lua
+++ lua/nvim/lazy.lua
@@ -21,5 +21,5 @@ require("lazy").setup({
   },
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = false },
 })
