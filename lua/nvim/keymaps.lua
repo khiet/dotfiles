da49b43440e00cdf46eed96a7f1065dcee5523d5
Khiet Le <khiet@outlook.com>
2025-07-24 18:32:08 +0100

    add <leader>yp to copy the file path

--- lua/nvim/keymaps.lua
+++ lua/nvim/keymaps.lua
@@ -66,6 +66,8 @@ set('n', '_', '<C-w>5-', opt_n)
 set('n', '-', '<C-w>5<', opt_n)
 set('n', '=', '<C-w>5>', opt_n)
 
+set("n", "<leader>yp", ":let @+ = expand('%:.')<CR>", opt_n)
+
 set("n", "<leader>ev", ':e <C-R>=expand($HOME."/dotfiles/lua/nvim/keymaps.lua")<CR><CR>', opt_n)
 set("n", "<leader>ez", ':e <C-R>=expand($HOME."/dotfiles/_zshrc")<CR><CR>', opt_n)
 set("n", "<leader>et", ':e <C-R>=expand($HOME."/dotfiles/tmux/_tmux.conf")<CR><CR>', opt_n)
