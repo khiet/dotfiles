7aede20d89e7d55edd71518dff30d26764112edc
Khiet Le <khiet@outlook.com>
2025-07-25 14:17:41 +0100

    use lazygit.vim for using lazygit

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,18 @@
+-- nvim v0.8.0
+return {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    keys = {
+        { "gl", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
+}
