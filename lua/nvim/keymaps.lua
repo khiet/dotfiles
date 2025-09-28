220f8f212e8af0a262b3caeee6189aed51a811d2
Khiet Le <khiet@outlook.com>
2025-09-28 11:37:24 +0100

    flip Yp and yp

--- lua/nvim/keymaps.lua
+++ lua/nvim/keymaps.lua
@@ -78,8 +78,8 @@ set('n', '_', '<C-w>5-', opt_n)
 set('n', '-', '<C-w>5<', opt_n)
 set('n', '=', '<C-w>5>', opt_n)
 
-set("n", "<leader>yp", ":let @+ = expand('%:.')<CR>", opt_n)
-set("n", "<leader>Yp", ":let @+ = expand('%:.') . ':' . line('.')<CR>", opt_n)
+set("n", "<leader>Yp", ":let @+ = expand('%:.')<CR>", opt_n)
+set("n", "<leader>yp", ":let @+ = expand('%:.') . ':' . line('.')<CR>", opt_n)
 
 set("n", "<leader>ev", ':e <C-R>=expand($HOME."/dotfiles/lua/nvim/keymaps.lua")<CR><CR>', opt_n)
 set("n", "<leader>ez", ':e <C-R>=expand($HOME."/dotfiles/_zshrc")<CR><CR>', opt_n)
