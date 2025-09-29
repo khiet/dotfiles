c8618bbf790ae7a6ddda54cbb5ec89de7e4d5119
Khiet Le <khiet@outlook.com>
2025-09-29 09:23:06 +0100

    update aws.zsh

--- zshrc/aws.zsh
+++ zshrc/aws.zsh
@@ -1,8 +1,9 @@
 alias aws_ssh='aws ssm start-session --document-name AWS-StartInteractiveCommand --parameters command="bash -l" --target'
-# connect to an instance protected from scale in in the specified auto scaling group
-
-# set ProtectedFromScaleIn to true
-# aws autoscaling set-instance-protection --instance-ids <instance-id> --auto-scaling-group-name <auto-scaling-group-name> --protected-from-scale-in
-# aws_ssh target should be $(aws autoscaling describe-auto-scaling-instances | jq -r '.AutoScalingInstances[] | select(.ProtectedFromScaleIn == true and .AutoScalingGroupName == "awseb-e-363wrq8hu8-stack-AWSEBAutoScalingGroup-1gC39UdauRcl") | .InstanceId')
 
 alias aws_ls_ec2='aws ec2 describe-instances --filters Name=instance-state-name,Values=running --query "Reservations[*].Instances[*].{Instance:InstanceId,Type:InstanceType,State:State.Name,Name:Tags[?Key==\`Name\`]|[0].Value}" --output table'
+
+# list instances protected from scale in
+# aws autoscaling describe-auto-scaling-instances --query "AutoScalingInstances[?ProtectedFromScaleIn==\`true\`].[InstanceId,AutoScalingGroupName]" --output table
+
+# set instance scale-in protection
+# EC2 > Auto Scaling groups > Instance management > Actions > Set scale-in protection
