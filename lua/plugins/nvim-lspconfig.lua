49ae17f2843c7c9c0701ac3f1b0fe6514fef68d1
Khiet Le <khiet@outlook.com>
2025-11-18 08:37:17 +0000

    modernize LSP config for Neovim 0.11 best practices

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -61,27 +61,38 @@ return {
         }),
       })
 
-      local on_attach = function(_, bufnr)
-        vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", { clear = true }),
+        callback = function(args)
+          local bufnr = args.buf
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if not client then return end
 
-        local bufopts = { noremap = true, silent = true, buffer = bufnr }
-        vim.keymap.set('n', '<leader>f', function()
-          vim.lsp.buf.format()
-          vim.cmd('write')
-        end, bufopts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-        vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, bufopts)
-        vim.keymap.set('n', 'gc', vim.lsp.buf.code_action, bufopts)
-        vim.keymap.set("n", "gi", function()
-          vim.lsp.buf.code_action({
-            context = { only = { "source.addMissingImports.ts" } },
-            apply = true,
-          })
-        end, { desc = "Auto import missing imports" })
-      end
+          -- set omnifunc for LSP completion
+          vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+          -- LSP keymaps
+          local bufopts = { noremap = true, silent = true, buffer = bufnr }
+          vim.keymap.set('n', '<leader>f', function()
+            vim.lsp.buf.format()
+            vim.cmd('write')
+          end, bufopts)
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+          vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, bufopts)
+          vim.keymap.set('n', 'gc', vim.lsp.buf.code_action, bufopts)
+          vim.keymap.set("n", "gi", function()
+            vim.lsp.buf.code_action({
+              context = { only = { "source.addMissingImports.ts" } },
+              apply = true,
+            })
+          end, { desc = "Auto import missing imports" })
+        end,
+      })
+
+      -- get LSP capabilities from cmp-nvim-lsp for enhanced completion
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
       -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-      -- Migrated to vim.lsp.config (Neovim 0.11+)
       local servers = {
         'ts_ls',
         'eslint',
@@ -89,13 +100,13 @@ return {
         'lua_ls',
       }
       for _, server in ipairs(servers) do
-        vim.lsp.config(server, { on_attach = on_attach })
+        vim.lsp.config(server, { capabilities = capabilities })
         vim.lsp.enable(server)
       end
 
       -- npm install -g typescript
       vim.lsp.config('ruby_lsp', {
-        on_attach = on_attach,
+        capabilities = capabilities,
         filetypes = { "ruby" },
         init_options = {
           addonSettings = {
