f58aacc6387849f2d979dd892e8e0f3073eec134
Khiet Le <khiet@outlook.com>
2025-11-26 17:04:43 +0000

    refactor toggleterm to use factory function for terminal toggles

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -16,28 +16,23 @@ return {
 
       -- https://github.com/akinsho/toggleterm.nvim?tab=readme-ov-file#custom-terminals
       local Terminal = require('toggleterm.terminal').Terminal
-      local lazygit  = Terminal:new({
-        cmd = "lazygit",
-        direction = "float",
-        hidden = true
-      })
 
-      function _lazygit_toggle()
-        lazygit:toggle()
+      local function create_terminal_toggle(cmd)
+        local term = Terminal:new({
+          cmd = cmd,
+          direction = "float",
+          hidden = true
+        })
+        return function()
+          term:toggle()
+        end
       end
 
-      local lazydocker  = Terminal:new({
-        cmd = "lazydocker",
-        direction = "float",
-        hidden = true
-      })
+      _lazygit_toggle = create_terminal_toggle("lazygit")
+      _lazydocker_toggle = create_terminal_toggle("lazydocker")
 
-      function _lazydocker_toggle()
-        lazydocker:toggle()
-      end
-
-      vim.keymap.set('n', 'tt', ":ToggleTerm direction=float<CR>", { silent = true, noremap = true })
-      vim.keymap.set('n', 'tg', "<cmd>lua _lazygit_toggle()<CR>", { silent = true, noremap = true })
+      vim.keymap.set('n', 'tt', ":ToggleTerm direction=float<CR>",   { silent = true, noremap = true })
+      vim.keymap.set('n', 'tg', "<cmd>lua _lazygit_toggle()<CR>",    { silent = true, noremap = true })
       vim.keymap.set('n', 'td', "<cmd>lua _lazydocker_toggle()<CR>", { silent = true, noremap = true })
     end
   }
