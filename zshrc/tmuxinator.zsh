537d5b0f719a58eb1a388ac8e923aec17439f363
Khiet Le <khiet@outlook.com>
2025-12-19 08:45:34 +0000

    set mux alias without conditional

--- zshrc/tmuxinator.zsh
+++ zshrc/tmuxinator.zsh
@@ -1,4 +1,2 @@
-if which tmuxinator &>/dev/null; then
-  alias mux=tmuxinator
-  export TMUXINATOR_CONFIG=$DEVS_HOME/tmuxinator
-fi
+alias mux=tmuxinator
+export TMUXINATOR_CONFIG=$DEVS_HOME/tmuxinator
