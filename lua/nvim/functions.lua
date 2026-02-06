5fb01c02b87c6b6d95797fe90627d198e2357494
Khiet Le <khiet@outlook.com>
2026-02-06 09:24:03 +0000

    clean up functions.lua: make functions local, remove unused utilities

--- lua/nvim/functions.lua
+++ lua/nvim/functions.lua
@@ -1,27 +1,8 @@
-function delete_trailing_whitespaces()
+local function delete_trailing_whitespaces()
   vim.cmd('%s/\\s\\+$//e')
 end
 
-local function replace_all(replacements)
-  for _, r in ipairs(replacements) do
-    if has_character(r.check or r.pattern) then
-      vim.cmd('%s/' .. r.pattern .. '/' .. r.replacement .. '/g')
-    end
-  end
-end
-
-function convert_to_ascii()
-  replace_all({
-    { pattern = "[''']", replacement = "'" },
-    { pattern = '["""]', replacement = '"' },
-    { pattern = '）', replacement = ')' },
-    { pattern = '（', replacement = '(' },
-    { pattern = '—', replacement = '-' },
-    { pattern = '…', replacement = '...' },
-  })
-end
-
-function has_character(char)
+local function has_character(char)
   local character = char
 
   local current_buffer = vim.api.nvim_get_current_buf()
@@ -36,32 +17,26 @@ function has_character(char)
   return false
 end
 
-function pretty_print_table(tbl, indent)
-  if not indent then indent = 0 end
-
-  for k, v in pairs(tbl) do
-    formatting = string.rep("  ", indent) .. k .. ": "
-
-    if type(v) == "table" then
-      print(formatting)
-      pretty_print_table(v, indent + 1)
-    elseif type(v) == "boolean" then
-      print(formatting .. tostring(v))
-    else
-      print(formatting .. v)
+local function replace_all(replacements)
+  for _, r in ipairs(replacements) do
+    if has_character(r.check or r.pattern) then
+      vim.cmd('%s/' .. r.pattern .. '/' .. r.replacement .. '/g')
     end
   end
 end
 
-function search_dictionary(lang)
-  if lang == 'en' then
-    os.execute('open "https://dictionary.cambridge.org/dictionary/english/' .. vim.fn.expand('<cword>') .. '"')
-  elseif lang == 'jp' then
-    os.execute('open "https://www.google.com/search?q=' .. vim.fn.expand('<cword>') .. '+意味"')
-  end
+local function convert_to_ascii()
+  replace_all({
+    { pattern = "[''']", replacement = "'" },
+    { pattern = '["""]', replacement = '"' },
+    { pattern = '）', replacement = ')' },
+    { pattern = '（', replacement = '(' },
+    { pattern = '—', replacement = '-' },
+    { pattern = '…', replacement = '...' },
+  })
 end
 
-function create_spec_file()
+local function create_spec_file()
   local cmd = '!ruby ~/dotfiles/scripts/create_spec_file.rb ' .. vim.fn.expand("%:p")
   vim.api.nvim_command("silent execute '" .. cmd .. "'")
 end
