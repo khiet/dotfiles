4f73646f75fbf7d34c06743e92a59d21cd9640d2
Khiet Le <khiet@outlook.com>
2026-02-24 07:43:32 +0000

    update copilot settings

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,23 +2,18 @@ return {
   {
     "github/copilot.vim",
     config = function()
-      -- Enable copilot for all file types by default
-      vim.g.copilot_enabled = true
-
-      -- Enable copilot for git commit messages
+      -- Eisable copilot for some file types
       vim.g.copilot_filetypes = {
-        gitcommit = true
+        ["*"] = false,
+        gitcommit = true,
+        markdown = true,
       }
 
-      -- Set up key mappings
       vim.keymap.set('i', '<C-j>', 'copilot#Accept("\\<CR>")', {
         expr = true,
         replace_keycodes = false
       })
       vim.g.copilot_no_tab_map = true
-
-      -- Toggle copilot enable/disable
-      vim.keymap.set('n', '<leader>ct', ':Copilot toggle<CR>', { noremap = true, silent = true })
     end
   }
 }
