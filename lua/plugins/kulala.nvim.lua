68188a74fa7b4a739ad9a03045cdce76b3192f17
Khiet Le <khiet@outlook.com>
2026-03-27 13:06:02 +0000

    set max_response_size to 10MB

--- lua/plugins/kulala.nvim.lua
+++ lua/plugins/kulala.nvim.lua
@@ -2,16 +2,16 @@
 return {
   "mistweaverco/kulala.nvim",
   keys = {
-    { "<leader>Rr", function() require("kulala").run() end, desc = "Send request" },
+    { "<leader>Rr", function() require("kulala").run() end,              desc = "Send request" },
     { "<leader>Re", function() require("kulala").set_selected_env() end, desc = "Select environment" },
   },
   ft = { "http", "rest" },
   opts = {
-    max_response_size = 10485760, -- 10MB
     global_keymaps = false,
     kulala_keymaps_prefix = "",
     ui = {
       -- https://neovim.getkulala.net/docs/getting-started/configuration-options#uiwin_opts
+      max_response_size = 10485760, -- 10MB
       win_opts = {
         wo = { foldenable = false },
       },
