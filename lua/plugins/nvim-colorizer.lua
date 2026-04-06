09a0a2de7f3fa5c283078e9be3049a9bfe65a84f
Khiet Le <khiet@outlook.com>
2026-04-06 21:09:36 +0100

    use better-maintained colourizer

--- lua/plugins/nvim-colorizer.lua
+++ lua/plugins/nvim-colorizer.lua
@@ -1,6 +1,22 @@
 return {
-  "norcalli/nvim-colorizer.lua",
+  "catgoose/nvim-colorizer.lua",
+  event = "BufReadPre",
+  opts = {},
   config = function()
-    require('colorizer').setup()
+    require("colorizer").setup({
+      filetypes = {
+        "*",
+        "!markdown",
+      },
+      options = {
+        parsers = {
+          css = true,
+          tailwind = {
+            enable = true,
+            lsp = true,
+          },
+        },
+      },
+    })
   end
 }
