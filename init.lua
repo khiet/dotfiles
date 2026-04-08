8221eaa4726be804a276472608326a44dea6924c
Khiet Le <khiet@outlook.com>
2026-04-08 16:01:08 +0100

    replace vim_current_word with vim-illuminate

--- init.lua
+++ init.lua
@@ -10,7 +10,3 @@ vim.o.ch = 1
 -- highlight trailing whitespaces
 vim.cmd([[au BufWritePre * match ExtraWhitespace /\s\+$/]])
 vim.cmd("hi ExtraWhitespace guibg=#ff5555")
-
--- vim_current_word
-vim.cmd("hi CurrentWord gui=underline")
-vim.cmd("hi CurrentWordTwins gui=underline")
