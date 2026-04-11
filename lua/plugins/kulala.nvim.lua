5a921cce092057ab82c5ee2f468c840b3bb3e1b5
Khiet Le <khiet@outlook.com>
2026-04-11 10:27:01 +0100

    use command mappings for kulala leader keys

--- lua/plugins/kulala.nvim.lua
+++ lua/plugins/kulala.nvim.lua
@@ -2,8 +2,8 @@
 return {
   "mistweaverco/kulala.nvim",
   keys = {
-    { "<leader>rr", function() require("kulala").run() end,              desc = "Send request",       ft = { "http", "rest" } },
-    { "<leader>re", function() require("kulala").set_selected_env() end, desc = "Select environment", ft = { "http", "rest" } },
+    { "<leader>rr", "<cmd>lua require('kulala').run()<CR>",              desc = "Send request",       ft = { "http", "rest" } },
+    { "<leader>re", "<cmd>lua require('kulala').set_selected_env()<CR>", desc = "Select environment", ft = { "http", "rest" } },
   },
   ft = { "http", "rest" },
   opts = {
