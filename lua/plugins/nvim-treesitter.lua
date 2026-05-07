e8e1855baa979c62c0f5a2508f46e72b2c099617
Khiet Le <khiet@outlook.com>
2026-05-07 14:57:08 +0100

    keep treesitter parser builds off startup

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,30 +1,38 @@
+local parsers = {
+  "c",
+  "lua",
+  "vim",
+  "vimdoc",
+  "query",
+  "ruby",
+  "embedded_template",
+  "javascript",
+  "typescript",
+  "tsx",
+  "xml",
+  "json",
+  "markdown",
+}
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
     branch = "main",
     lazy = false,
-    build = ":TSUpdate",
+    -- After changing `parsers`, run `:Lazy build nvim-treesitter` to install
+    -- missing parsers and update existing ones. Removed parsers must be
+    -- uninstalled manually with `:TSUninstall <parser>`.
+    build = function()
+      local treesitter = require("nvim-treesitter")
+
+      treesitter.install(parsers):wait(300000)
+      treesitter.update(parsers):wait(300000)
+    end,
     config = function()
       local treesitter = require("nvim-treesitter")
 
       treesitter.setup({
         install_dir = vim.fn.stdpath("data") .. "/site",
-      })
-
-      treesitter.install({
-        "c",
-        "lua",
-        "vim",
-        "vimdoc",
-        "query",
-        "ruby",
-        "embedded_template",
-        "javascript",
-        "typescript",
-        "tsx",
-        "xml",
-        "json",
-        "markdown",
       })
 
       vim.treesitter.language.register("embedded_template", "eruby")
