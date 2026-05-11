478a7a568dd45389ead258348e5e4cb3808ae1c9
Khiet Le <khiet@outlook.com>
2026-05-11 11:38:08 +0100

    fix: handle spaces in file helper paths

--- zshrc/public/functions/3_files.zsh
+++ zshrc/public/functions/3_files.zsh
@@ -1,2 +1,2 @@
-function mv2 { dirname "$2" | xargs mkdir -p; mv "$1" "$2" }
-function cp2 { dirname "$2" | xargs mkdir -p; cp "$1" "$2" }
+function mv2 { mkdir -p "$(dirname "$2")"; mv "$1" "$2" }
+function cp2 { mkdir -p "$(dirname "$2")"; cp "$1" "$2" }
