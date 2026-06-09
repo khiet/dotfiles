a7844087a8905e35f94011dffa11f90730928a6d
Khiet Le <khiet@outlook.com>
2026-06-09 18:49:29 +0100

    fix: keep lualine context readable

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,6 +1,20 @@
 return {
   "nvim-lualine/lualine.nvim",
   config = function()
+    local function project_relative_path()
+      local file = vim.fn.expand('%:p')
+
+      local git_root = vim.fn.systemlist(
+        'git rev-parse --show-toplevel'
+      )[1]
+
+      if git_root and git_root ~= '' then
+        return file:sub(#git_root + 2)
+      end
+
+      return vim.fn.expand('%:.')
+    end
+
     require('lualine').setup {
       options = {
         icons_enabled = true,
@@ -8,12 +22,20 @@ return {
       },
       sections = {
         lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = {
+        lualine_b = {
           {
-            'filename',
-            path = 3
-          }
+            'branch',
+            fmt = function(branch)
+              return #branch > 19
+                  and branch:sub(1, 17) .. '..'
+                  or branch
+            end,
+          },
+          'diff',
+          'diagnostics',
+        },
+        lualine_c = {
+          project_relative_path,
         },
         lualine_x = { 'encoding', 'filetype' },
         lualine_y = { 'progress' },
