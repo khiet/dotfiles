2aae758cf9bd01965e34dd77eb219dd4fad9dc41
Khiet Le <khiet@outlook.com>
2026-06-11 22:11:00 +0100

    add ssm_tunnel in aws.zsh

--- zshrc/public/aws.zsh
+++ zshrc/public/aws.zsh
@@ -1,9 +1,21 @@
-alias aws_ssh='aws ssm start-session --document-name AWS-StartInteractiveCommand --parameters command="bash -l" --target'
+alias ssm_shell='aws ssm start-session --document-name AWS-StartInteractiveCommand --parameters command="bash -l" --target'
 
 alias aws_ls_ec2='aws ec2 describe-instances --filters Name=instance-state-name,Values=running --query "Reservations[*].Instances[*].{Instance:InstanceId,Type:InstanceType,State:State.Name,Name:Tags[?Key==\`Name\`]|[0].Value}" --output table'
 
-# list instances protected from scale in
-# aws autoscaling describe-auto-scaling-instances --query "AutoScalingInstances[?ProtectedFromScaleIn==\`true\`].[InstanceId,AutoScalingGroupName]" --output table
-
-# set instance scale-in protection
-# EC2 > Auto Scaling groups > Instance management > Actions > Set scale-in protection
+# aws_tunnel <instance-id> <remote-host> [remote-port] [local-port]
+# Forwards a local port through an SSM-managed EC2 instance to a remote host
+# (e.g. an RDS endpoint that isn't publicly reachable).
+aws_tunnel() {
+  local target="$1"
+  local host="$2"
+  local rport="${3:-5432}"
+  local lport="${4:-$rport}"
+  if [[ -z "$target" || -z "$host" ]]; then
+    echo "usage: aws_tunnel <instance-id> <remote-host> [remote-port] [local-port]" >&2
+    return 1
+  fi
+  aws ssm start-session \
+    --document-name AWS-StartPortForwardingSessionToRemoteHost \
+    --parameters host="$host",portNumber="$rport",localPortNumber="$lport" \
+    --target "$target"
+}
