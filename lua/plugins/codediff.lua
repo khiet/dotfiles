bc64f7a2f59483fdd36cf8500fca03add1d4bcc3
Khiet Le <khiet@outlook.com>
2026-06-20 15:05:17 +0100

    add codediff

---
+++ lua/plugins/codediff.lua
@@ -0,0 +1,15 @@
+return {
+  "esmuellert/codediff.nvim",
+  cmd = "CodeDiff",
+  opts = {
+    explorer = {
+      auto_open_on_cursor = true
+    },
+  },
+  init = function()
+    local opt_sn = { silent = true, noremap = true }
+    vim.keymap.set('n', '<leader>ds', ':CodeDiff<CR>', opt_sn)
+    vim.keymap.set('n', '<leader>dm', ':CodeDiff main<CR>', opt_sn)
+    vim.keymap.set('n', '<leader>dh', ':CodeDiff HEAD~<CR>', opt_sn)
+  end,
+}
