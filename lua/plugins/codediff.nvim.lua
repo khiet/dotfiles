9e4600bbe11c39d006ac40603ef8082a2e8e8ad1
Khiet Le <khiet@outlook.com>
2026-06-21 08:14:15 +0100

    feat: add codediff clipboard history mapping

--- lua/plugins/codediff.nvim.lua
+++ lua/plugins/codediff.nvim.lua
@@ -11,5 +11,12 @@ return {
     vim.keymap.set('n', '<leader>ds', ':CodeDiff<CR>', opt_sn)
     vim.keymap.set('n', '<leader>dm', ':CodeDiff main<CR>', opt_sn)
     vim.keymap.set('n', '<leader>dh', ':CodeDiff HEAD~<CR>', opt_sn)
+    vim.keymap.set('n', '<leader>dc', function()
+      local ref = vim.fn.getreg('+'):match('^%s*(.-)%s*$')
+
+      if ref ~= '' then
+        vim.cmd('CodeDiff history ' .. vim.fn.fnameescape(ref))
+      end
+    end, opt_sn)
   end,
 }
