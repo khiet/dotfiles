3fb0f3c8de1954d7342bd2e822a102528f86519e
Khiet Le <khiet@outlook.com>
2026-09-10 10:50:09 +0100

    fix(nvim): remove Gitsigns mappings that override CodeDiff navigation

--- lua/plugins/gitsigns.nvim.lua
+++ lua/plugins/gitsigns.nvim.lua
@@ -1,35 +1,14 @@
 return {
   {
-    "lewis6991/gitsigns.nvim",
+    'lewis6991/gitsigns.nvim',
     config = function()
-      require('gitsigns').setup {
+      require('gitsigns').setup({
         signs = {
-          add    = { text = '+' },
+          add = { text = '+' },
           change = { text = '~' },
           delete = { text = '_' },
         },
-        on_attach = function(bufnr)
-          local gs = package.loaded.gitsigns
-
-          local function map(mode, l, r, opts)
-            opts = opts or {}
-            opts.buf = bufnr
-            vim.keymap.set(mode, l, r, opts)
-          end
-
-          map('n', ']c', function()
-            if vim.wo.diff then return ']c' end
-            vim.schedule(function() gs.next_hunk() end)
-            return '<Ignore>'
-          end, { expr = true })
-
-          map('n', '[c', function()
-            if vim.wo.diff then return '[c' end
-            vim.schedule(function() gs.prev_hunk() end)
-            return '<Ignore>'
-          end, { expr = true })
-        end
-      }
-    end
-  }
+      })
+    end,
+  },
 }
