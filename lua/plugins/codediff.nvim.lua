dd3059822617061ff9762cd66cbf219c40fa3515
Khiet Le <khiet@outlook.com>
2026-09-10 09:59:17 +0100

    fix(nvim): compare clipboard commit against working tree

--- lua/plugins/codediff.nvim.lua
+++ lua/plugins/codediff.nvim.lua
@@ -25,7 +25,7 @@ return {
       local ref = vim.fn.getreg('+'):match('^%s*(.-)%s*$')
 
       if ref ~= '' then
-        vim.cmd('CodeDiff ' .. vim.fn.fnameescape(ref .. '^') .. ' ' .. vim.fn.fnameescape(ref))
+      vim.cmd('CodeDiff ' .. vim.fn.fnameescape(ref))
       end
     end, opt_sn)
   end,
